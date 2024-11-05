04478f96c903b08003f437700ac69deadb0b665d
RuiGene <zrdanielyang@gmail.com>
2024-11-05 18:44:20 +1300

    Turned traffic data into function

--- data_extraction/traffic.py
+++ data_extraction/traffic.py
@@ -34,7 +34,7 @@ url = 'https://maps.googleapis.com/maps/api/distancematrix/json'
 origin = home_address
 destination = [
     '-36.85581, 174.76637', # University Address
-    '-36.84801, 174.7578'
+    '-36.84801, 174.7578' # Work Address
 ]
 destination_str = '|'.join(destination)
 
@@ -46,21 +46,23 @@ params = {
     'key': GOOGLE_KEY
 }
 
-response = requests.get(url, params = params)
+def get_traffic_data(url, params):
+    response = requests.get(url, params = params)
 
-results = []
+    results = []
 
-data = response.json()
+    data = response.json()
 
-for i, element in enumerate(data["rows"][0]["elements"]):
-    try:
-        travel_time = element["duration_in_traffic"]["text"]
-        
-        destination_info = {
-            'destination_name': data['destination_addresses'][i],
-            'time_taken': travel_time
-        }
-        results.append(destination_info)
-        
-    except KeyError:
-        print(f"Destination {i+1} - Error: Could not retrieve travel time with traffic.")
+    for i, element in enumerate(data["rows"][0]["elements"]):
+        try:
+            travel_time = element["duration_in_traffic"]["text"]
+            
+            destination_info = {
+                'destination_name': data['destination_addresses'][i],
+                'time_taken': travel_time
+            }
+            results.append(destination_info)
+            
+        except KeyError:
+            return None
+    return results
